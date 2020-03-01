93aaf9479ecc078e29f013a0db349c8f423981f2
dbunavi <dbunavi@mymail.mines.edu>
2020-03-01 16:02:05 -0700

    added new test classes

---
+++ src/tests/TestsPart3.java
@@ -0,0 +1,22 @@
+//Authors: Elizabeth Bauch and Danella Bunavi
+
+package tests;
+
+import java.util.Set;
+
+//Doing a static import allows me to write assertEquals rather than
+//assertEquals
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+
+public class TestsPart3 {
+
+	public TestsPart3() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
