bce3afc7b9eb6bdce9b9c2c54dd7469bd1f042b6
Elizabeth Bauch <ebauch@mymail.mines.edu>
2020-03-12 00:08:00 -0600

    Had to fix test that I broke

--- src/tests/TestsPart1.java
+++ src/tests/TestsPart1.java
@@ -33,7 +33,7 @@ public class TestsPart1 {
 		// Initialize will load BOTH config files 
 		board.initialize();
 	}
-	
+
 	@Test
 	public void testRoomsAndLegendFile() {
 		// Get the map of initial => room 
@@ -45,10 +45,10 @@ public class TestsPart1 {
 		assertEquals("Conference Room", legend.get('C'));
 		assertEquals("Warehouse", legend.get('H'));
 		assertEquals("Kitchen", legend.get('K'));
-		assertEquals("Michael’s House", legend.get('M'));
-		assertEquals("Dwight’s Desk", legend.get('D'));
+		assertEquals("Michaelâ€™s House", legend.get('M'));
+		assertEquals("Dwightâ€™s Desk", legend.get('D'));
 	}
-	
+
 	@Test
 	public void testRowsAndColumns() {
 		// Ensure we have the proper number of rows and columns
